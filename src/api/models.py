8409db15a746a747889cf99d7e649c762c41945b
onecodestar <onecodestar@outlook.com>
2023-07-10 19:46:07 +0900

    update API

--- src/api/models.py
+++ src/api/models.py
@@ -5,19 +5,6 @@ from sqlalchemy.orm import relationship, column_property
 from sqlalchemy.sql import select
 
 
-class Item(Base):
-    __tablename__='items'
-    id=Column(Integer,primary_key=True)
-    name=Column(String(255),nullable=False,unique=True)
-    description=Column(Text)
-    price=Column(Integer,nullable=False)
-    on_offer=Column(Boolean,default=False)
-
-
-    def __repr__(self):
-        return f"<Item name={self.name} price={self.price}>"
-
-
 class Book(Base):
     __tablename__='books'
     id=Column(Integer,primary_key=True, autoincrement=True)
