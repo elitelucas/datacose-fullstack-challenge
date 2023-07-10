26e45b68c4d9599030223e012fe23b03d44a0ead
webdev <webdev181011@gmail.com>
2023-07-10 19:24:44 +0900

    update home page

--- src/dashboard/pages/index.vue
+++ src/dashboard/pages/index.vue
@@ -1,67 +1,14 @@
 <template>
   <b-row style="margin-left: 0;margin-right: 0;">
-    <b-col md="4">
-      <b-img
-        :src="avatar"
-        id="avatar-img"
-        alt="Asaolu Elijah Memoji"
-        @mouseenter="changeAvatar"
-        @mouseleave="changeAvatar"
-      />
-    </b-col>
+    <b-col md="4"> </b-col>
 
     <b-col md="8">
       <h2 class="intro animate__animated animate__fadeInUp animate__fast">
         Hi 👋,
-        <br />I<span>'m Asaolu&nbsp;Elijah</span>.
+        <br />I<span>'m Lucas Wang</span>.
       </h2>
-
       <div class="col-md-10 info">
-        A <b>front-end developer</b> and <b>technical writer</b> passionate
-        about solving problems with tech.<br />
-        I am skilled with building exceptional <b>websites</b> and
-        <b>mobile applications</b>, and currently i'm exploring the AI
-        (Artificial Intelligence) ecosystem.
-        <br />
-        <b-button
-          class="action-btn animate__animated animate__wobble animate__delay-4s animate__fast"
-          to="/projects"
-        >
-          Explore >
-        </b-button>
-
-        <b-button class="action-btn" to="/contact" variant="primary">
-          Get In Touch
-        </b-button>
-
-        <!-- .SOCIAL LINKS SECTION -->
-        <div
-          class="social-icons animate__animated animate__fadeInUp animate__delay-1s"
-        >
-          <social-link :to="socialLinks.github">
-            <GithubIcon />
-          </social-link>
-
-          <social-link :to="socialLinks.linkedin">
-            <LinkedinIcon />
-          </social-link>
-
-          <social-link :to="socialLinks.twitter">
-            <TwitterIcon />
-          </social-link>
-
-          <social-link :to="socialLinks.facebook">
-            <FacebookIcon />
-          </social-link>
-
-          <social-link :to="socialLinks.mail">
-            <MailIcon />
-          </social-link>
-
-          <social-link :to="socialLinks.youtube">
-            <YoutubeIcon />
-          </social-link>
-        </div>
+        <p>This is project for DataCose fullstack challenge</p>
       </div>
     </b-col>
   </b-row>
@@ -101,23 +48,23 @@ export default {
   },
 
   head: {
-    title: "Portfolio ⚡ - Asaolu Elijah",
+    title: "Portfolio ⚡ - Lucas Wang",
     meta: [
       {
         hid: "description",
         name: "description",
         content:
-          "Asaolu Elijah is an awesome front-end developer and technical writer, passionate about building solutions that works across multiple platforms with high accessibility approach. Elijah has vast knowledge in web development with over 3+ years experience in building actual products"
+          "Lucas Wang is an awesome front-end developer and technical writer, passionate about building solutions that works across multiple platforms with high accessibility approach. Elijah has vast knowledge in web development with over 3+ years experience in building actual products"
       },
       {
         hid: "og:title",
         name: "og:title",
-        content: "Portfolio ⚡ - Asaolu Elijah"
+        content: "Portfolio ⚡ - Lucas Wang"
       },
       {
         property: "og:description",
         content:
-          "Asaolu Elijah is an awesome front-end developer and technical writer, passionate about building solutions that works across multiple platforms with high accessibility approach. Elijah has vast knowledge in web development with over 3+ years experience in building actual products"
+          "Lucas Wang is an awesome front-end developer and technical writer, passionate about building solutions that works across multiple platforms with high accessibility approach. Elijah has vast knowledge in web development with over 3+ years experience in building actual products"
       },
       {
         hid: "og:image",
